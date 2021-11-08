bcaab09a2e81dd00f41d659fc90aaecc4c286178
khoironi2 <khoironi7887@students.amikom.ac.id>
2021-11-08 17:28:45 +0700

    sa

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/dispora/';
+$config['base_url'] = 'https://darimu.garisdesign.com/';
 
 /*
 |--------------------------------------------------------------------------
