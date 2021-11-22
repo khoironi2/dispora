2ba82c3264849f1d6a22a7800c64260418243bb4
khoironi2 <khoironi7887@students.amikom.ac.id>
2021-11-22 21:55:42 +0700

    tes

--- application/views/disc/replychat.php
+++ application/views/disc/replychat.php
@@ -245,8 +245,8 @@
                             </div>
                         </div>
 
-                        <div class="col-auto">
-                            <button type="submit" class="btn btn-komen btn-icon btn-primary rounded-circle ms-5">
+                        <div class="col-auto btn-komen ">
+                            <button type="submit" class="btn  btn-icon btn-primary rounded-circle ms-5">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-send">
                                     <line x1="22" y1="2" x2="11" y2="13"></line>
                                     <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
